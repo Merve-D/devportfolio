50901c0cec7fc198e279752f3d94dbf94dffa23d
Merve Dönmez <donmez1976merve228gmail.com>
2026-07-18 02:14:07 +0300

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://merve-d.github.io",
+  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
